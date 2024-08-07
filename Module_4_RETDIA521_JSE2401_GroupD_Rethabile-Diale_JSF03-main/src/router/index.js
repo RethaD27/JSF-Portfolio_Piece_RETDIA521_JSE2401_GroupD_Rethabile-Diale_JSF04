5162146f6c79f9f1386ac97bf31ebab63ea2c5bd
Rethabile Diale <rethaD27@gmail.com>
2024-08-07 14:27:11 +0200

    "Updated router/index.js: reformatted code, added ProtectedComponent, and implemented authentication check in beforeEach hook"

--- Module_4_RETDIA521_JSE2401_GroupD_Rethabile-Diale_JSF03-main/src/router/index.js
+++ Module_4_RETDIA521_JSE2401_GroupD_Rethabile-Diale_JSF03-main/src/router/index.js
@@ -1,43 +1,57 @@
-import { createRouter, createWebHistory } from "vue-router";
-import ProductGrid from "../components/ProductGrid.vue";
-import ProductDetail from "../components/ProductDetail.vue";
-import Login from "../components/Login.vue";
-import ShoppingCart from './components/ShoppingCart.vue';
-import { requireAuth } from "../auth";
+import { createRouter, createWebHistory } from 'vue-router';
+import ProductGrid from '../components/ProductGrid.vue';
+import ProductDetail from '../components/ProductDetail.vue';
+import Login from '../components/Login.vue';
+import ShoppingCart from '../components/ShoppingCart.vue';
+import ProtectedComponent from '../components/ProtectedComponent.vue';
+import { requireAuth } from '../auth';
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: ProductGrid,
   },
   {
-    path: "/product/:id",
-    name: "ProductDetail",
+    path: '/product/:id',
+    name: 'ProductDetail',
     component: ProductDetail,
     props: true,
   },
   {
-    path: "/login",
-    name: "Login",
+    path: '/login',
+    name: 'Login',
     component: Login,
   },
-  { 
-    path: '/cart', 
+  {
+    path: '/cart',
+    name: 'ShoppingCart',
     component: ShoppingCart,
-    beforeEnter: requireAuth
+    meta: { requiresAuth: true },
   },
   {
-    path: "/protected",
-    name: "Protected",
-    component: () => import("../components/ProtectedComponent.vue"),
-    beforeEnter: requireAuth,
+    path: '/protected',
+    name: 'Protected',
+    component: ProtectedComponent,
+    meta: { requiresAuth: true },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!localStorage.getItem('token')) {
+      next('/login');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
