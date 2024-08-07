dcb1e97adb829de1e6a5a518f622876ce8323b92
Rethabile Diale <rethaD27@gmail.com>
2024-08-07 21:27:59 +0200

    "Added ComparisonPage route to router"

---
+++ Module_4_RETDIA521_JSE2401_GroupD_Rethabile-Diale_JSF03-main/src/composables/useComparison.js
@@ -0,0 +1,37 @@
+// src/composables/useComparison.js
+import { ref, computed } from 'vue'
+
+const comparisonList = ref([])
+const MAX_COMPARISON_ITEMS = 4
+
+export function useComparison() {
+  const addToComparison = (product) => {
+    if (comparisonList.value.length < MAX_COMPARISON_ITEMS && !comparisonList.value.some(item => item.id === product.id)) {
+      comparisonList.value.push(product)
+    }
+  }
+
+  const removeFromComparison = (productId) => {
+    comparisonList.value = comparisonList.value.filter(item => item.id !== productId)
+  }
+
+  const clearComparison = () => {
+    comparisonList.value = []
+  }
+
+  const isInComparison = (productId) => {
+    return comparisonList.value.some(item => item.id === productId)
+  }
+
+  const comparisonCount = computed(() => comparisonList.value.length)
+
+  return {
+    comparisonList,
+    addToComparison,
+    removeFromComparison,
+    clearComparison,
+    isInComparison,
+    comparisonCount,
+    MAX_COMPARISON_ITEMS
+  }
+}
