25fe03ee5ca0e24d0ddb225ce16a0e7563b86aba
Rethabile Diale <rethaD27@gmail.com>
2024-08-08 11:30:45 +0200

    "Added Wishlist route and component, updated ComparisonPage route name, and reformatted code"

--- Module_4_RETDIA521_JSE2401_GroupD_Rethabile-Diale_JSF03-main/src/components/Navbar.vue
+++ Module_4_RETDIA521_JSE2401_GroupD_Rethabile-Diale_JSF03-main/src/components/Navbar.vue
@@ -1,10 +1,16 @@
 <template>
-  <nav class="fixed w-full bg-purple-900 p-4 text-purple-400 shadow-lg z-10">
-    <div class="container mx-auto flex justify-between items-center">
+  <nav
+    class="fixed w-full shadow-lg z-10"
+    :class="[
+      'bg-purple-900 text-purple-400',
+      { 'dark:bg-gray-800 dark:text-gray-400': isDarkMode }
+    ]"
+  >
+    <div class="container mx-auto flex justify-between items-center p-4">
       <div class="text-2xl font-bold swift-cart-store">SwiftCart Store</div>
       <div class="hidden md:flex">
         <router-link to="/" class="mr-4 hover:underline">Home</router-link>
-        <router-link to="/" class="mr-4 hover:underline">Wishlist</router-link>
+        <router-link to="/wishlist" v-if="isLoggedIn" class="mr-4 hover:underline">Wishlist</router-link>
         <router-link to="/cart" v-if="isLoggedIn" class="mr-4 hover:underline">
           Cart ({{ totalItems }})
         </router-link>
@@ -14,34 +20,46 @@
         <router-link v-if="!isLoggedIn" to="/login" class="hover:underline">Login</router-link>
         <button v-else @click="logout" class="hover:underline">Logout</button>
       </div>
-      <button @click="toggleMenu" class="md:hidden text-white">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16m-7 6h7"
-          />
-        </svg>
-      </button>
+      <div class="flex items-center">
+        <ThemeToggle @theme-changed="isDarkMode = $event" />
+        <button @click="toggleMenu" class="md:hidden text-white dark:text-gray-400 ml-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
-    <div v-if="open" class="md:hidden mt-4 bg-purple-500">
-      <router-link to="/" class="block p-2 hover:bg-yellow-400">Home</router-link>
-      <router-link to="/" class="block p-2 hover:bg-yellow-400">Wishlist</router-link>
-      <router-link to="/cart" v-if="isLoggedIn" class="block p-2 hover:bg-yellow-400">
+    <div
+      v-if="open"
+      class="md:hidden mt-4"
+      :class="[
+        'bg-purple-500 dark:bg-gray-700',
+        { 'dark:text-gray-400': isDarkMode }
+      ]"
+    >
+      <router-link to="/" class="block p-2 hover:bg-yellow-400 dark:hover:bg-gray-600">Home</router-link>
+      <router-link to="/wishlist" v-if="isLoggedIn" class="block p-2 hover:bg-yellow-400 dark:hover:bg-gray-600">Wishlist</router-link>
+      <router-link to="/cart" v-if="isLoggedIn" class="block p-2 hover:bg-yellow-400 dark:hover:bg-gray-600">
         Cart ({{ totalItems }})
       </router-link>
-      <router-link to="/comparison" v-if="isLoggedIn" class="block p-2 hover:bg-yellow-400">
+      <router-link to="/comparison" v-if="isLoggedIn" class="block p-2 hover:bg-yellow-400 dark:hover:bg-gray-600">
         Compare ({{ comparisonCount }})
       </router-link>
-      <router-link v-if="!isLoggedIn" to="/login" class="block p-2 hover:bg-yellow-400">Login</router-link>
-      <button v-else @click="logout" class="block w-full text-left p-2 hover:bg-yellow-400">Logout</button>
+      <router-link v-if="!isLoggedIn" to="/login" class="block p-2 hover:bg-yellow-400 dark:hover:bg-gray-600">Login</router-link>
+      <button v-else @click="logout" class="block w-full text-left p-2 hover:bg-yellow-400 dark:hover:bg-gray-600">
+        Logout
+      </button>
     </div>
   </nav>
 </template>
@@ -51,15 +69,20 @@ import { ref, inject } from 'vue'
 import { useRouter } from 'vue-router'
 import { useCart } from '../composables/useCart'
 import { useComparison } from '../composables/useComparison'
+import ThemeToggle from './ThemeToggle.vue'
 
 export default {
   name: 'Navbar',
+  components: {
+    ThemeToggle
+  },
   setup() {
     const open = ref(false)
     const router = useRouter()
     const isLoggedIn = inject('isLoggedIn')
     const { clearCart, totalItems } = useCart()
     const { comparisonCount } = useComparison()
+    const isDarkMode = ref(false)
 
     const toggleMenu = () => {
       open.value = !open.value
@@ -78,7 +101,8 @@ export default {
       isLoggedIn,
       logout,
       totalItems,
-      comparisonCount
+      comparisonCount,
+      isDarkMode
     }
   }
 }
@@ -87,7 +111,7 @@ export default {
 <style scoped>
 .swift-cart-store {
   font-family: 'Brush Script MT', cursive;
-  text-shadow: 2px 2px 4px rgba(0,0,0,0.3);
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
   letter-spacing: 1px;
   background: linear-gradient(45deg, #ff79c6, #bd93f9);
   -webkit-background-clip: text;
