2825e3315a8449c988d2812e92a124795ad09eb7
Rethabile <rethadiale27@gmail.com>
2024-08-15 11:31:43 +0200

    "Added wishlist feature to Home and Navbar components, implemented wishlist carousel, and updated useWishlist composable"

---
+++ src/components/WishlistCarousel.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-if="wishlist.length > 0" class="wishlist-carousel">
+      <h2>Your Wishlist</h2>
+      <!-- Implement a carousel with wishlist items -->
+    </div>
+  </template>
+  
+  <script>
+  import { useWishlist } from '../composables/useWishlist'
+  
+  export default {
+    setup() {
+      const { wishlist } = useWishlist()
+      return { wishlist }
+    }
+  }
+  </script>
